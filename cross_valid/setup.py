508b2f65c64074313aa7c2d1e08dd879c59f7686
Utkarsh Raj <43613722+Valdermaut@users.noreply.github.com>
2020-03-08 14:29:54 +0530

    Delete setup.py

--- cross_valid/setup.py
+++
@@ -1,3 +0,0 @@
-from setuptools import setup
-setup(name='cross_valid',version='0.1',description='A plugin for cropai_cross_validation',,author='utkarsh',author_email='utkarsh02dec@gmail.com',license='MIT',packages=['cross_valid'],zip_safe=False)
-
