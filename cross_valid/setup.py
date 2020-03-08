73b4af418507fa10ed642fb518cc565a5a37e868
Utkarsh Raj <43613722+Valdermaut@users.noreply.github.com>
2020-03-08 14:17:47 +0530

    Add files via upload

---
+++ cross_valid/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+setup(name='cross_valid',version='0.1',description='A plugin for cropai_cross_validation',url='https://github.com/snehilk1312',author='snehil',author_email='snehilkumar14072000@gmail.com',license='MIT',packages=['cross_valid'],zip_safe=False)
+
