7b7ac307b7b81381fee180db2d3b13abbf3123a5
Utkarsh Raj <43613722+Valdermaut@users.noreply.github.com>
2020-03-08 14:35:26 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+setup(name='cross_valid',version='0.1',description='A plugin for cropai_cross_validation',url='https://github.com/Valdermaut',author='Utkarsh',author_email='utkarsh02dec@gmail.com',license='MIT',packages=['cross_valid'],zip_safe=False)
+
