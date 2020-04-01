1bb25d04b4bb50974d8eb5381db4cb856ca4583b
Utkarsh Raj <43613722+Valdermaut@users.noreply.github.com>
2020-04-02 00:08:37 +0530

    Add files via upload

---
+++ cropai/modules/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+setup(name='cropai',version='0.1',description='A plugin for cropai',url='www.google.com',author='Your Name',author_email='utkarsh02dec@gmail.com',license='MIT',packages=['cropai'],zip_safe=False)
+
